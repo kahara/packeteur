dccc7c12085e720f2156844aaec52fdeb17232fa
Joni Kähärä <joni.kahara@async.fi>
2024-03-28 20:50:21 +0000

    receive packets

--- collect.go
+++ collect.go
@@ -1,5 +1,31 @@
 package main
 
-func collect(endpoint string) {
+import (
+	"github.com/rs/zerolog/log"
+	"net"
+)
 
+func collect(endpoint string) {
+	var (
+		err    error
+		addr   *net.UDPAddr
+		conn   *net.UDPConn
+		buf    []byte
+		count  int
+		source *net.UDPAddr
+	)
+
+	if addr, err = net.ResolveUDPAddr("udp", endpoint); err != nil {
+		log.Err(err)
+	}
+	if conn, err = net.ListenUDP("udp", addr); err != nil {
+		log.Err(err)
+	}
+
+	for {
+		if count, _, _, source, err = conn.ReadMsgUDP(buf, nil); err != nil {
+			log.Err(err)
+		}
+		log.Debug().Int("count", count).Any("source", source).Bytes("packet", buf).Msg("Packet received")
+	}
 }
