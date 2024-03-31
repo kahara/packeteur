0a1ff0e79c3079df79b9e0c7f11b387ccbf72ba9
Joni Kähärä <joni.kahara@async.fi>
2024-03-31 19:50:09 +0000

    poke gopacket.layers [skip ci]

--- generator.go
+++ generator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/rs/zerolog/log"
 	"math/rand"
 )
@@ -13,12 +14,64 @@ func toss() bool {
 	return true
 }
 
+// https://pkg.go.dev/github.com/google/gopacket#hdr-Creating_Packet_Data
+
 func generateTestPacket() []byte {
-	var packet []byte
+	var (
+		buf  = gopacket.NewSerializeBuffer()
+		opts = gopacket.SerializeOptions{}
+	)
 
-	x := gopacket.NewPacket(nil, gopacket.DecodeUnknown, gopacket.Default)
+	_ = gopacket.SerializeLayers(buf, opts,
+		&layers.Ethernet{
+			BaseLayer:    layers.BaseLayer{},
+			SrcMAC:       nil,
+			DstMAC:       layers.EthernetBroadcast,
+			EthernetType: 0,
+			Length:       0,
+		},
+		&layers.IPv4{
+			BaseLayer:  layers.BaseLayer{},
+			Version:    0,
+			IHL:        0,
+			TOS:        0,
+			Length:     0,
+			Id:         0,
+			Flags:      0,
+			FragOffset: 0,
+			TTL:        0,
+			Protocol:   17,
+			Checksum:   0,
+			SrcIP:      nil,
+			DstIP:      nil,
+			Options:    nil,
+			Padding:    nil,
+		},
+		&layers.TCP{
+			BaseLayer:  layers.BaseLayer{},
+			SrcPort:    0,
+			DstPort:    123,
+			Seq:        0,
+			Ack:        0,
+			DataOffset: 0,
+			FIN:        false,
+			SYN:        false,
+			RST:        false,
+			PSH:        false,
+			ACK:        false,
+			URG:        false,
+			ECE:        false,
+			CWR:        false,
+			NS:         false,
+			Window:     0,
+			Checksum:   0,
+			Urgent:     0,
+			Options:    nil,
+			Padding:    nil,
+		},
+		gopacket.Payload([]byte{73, 86}))
 
-	log.Debug().Any("x", x).Msg("")
+	log.Debug().Any("buf", buf).Msg("")
 
-	return packet
+	return buf.Bytes()
 }
